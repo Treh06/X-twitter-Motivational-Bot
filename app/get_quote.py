ecf7828f09f5ad50e3060ceede2011c4159ceb50
Treh06 <Twahreh10@gmail.com>
2025-03-11 00:16:48 -0500

    Enhance quote retrieval and tweeting functionality with improved comments and structure

--- app/get_quote.py
+++ app/get_quote.py
@@ -1,16 +1,22 @@
 import csv
 import os
 
+# Get the base directory of the project
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# Define the path to the quotes CSV file
 file_path = os.path.join(BASE_DIR, "data", "quotes.csv")
-
 
 def get_quote():
     quotes = []
+    # Open the CSV file containing quotes
     with open(file_path, 'r', newline='', encoding='utf-8') as quotes_file:
         reader = csv.reader(quotes_file)
+        # Read each row in the CSV file
         for row in reader:
+            # Ensure the row has exactly 2 elements (author and quote)
             if len(row) == 2:
                 author, quote = row
+                # Append the quote to the list as a dictionary
                 quotes.append({'author': author, 'quote': quote})
+    # Return the list of quotes
     return quotes
--- app/main.py
+++ app/main.py
@@ -3,21 +3,25 @@ from get_quote import get_quote
 import time
 import random
 import tweepy
-
-
+from datetime import datetime, timedelta
 
 def main():
-   # Connect to Twitter
+    # Connect to Twitter
     client = get_twitter_conn()
 
     try:
+        # Get a random quote
         random_quotes = random.choice(get_quote())
 
+        # Format the quote
         quote = f"'{random_quotes['quote']}' - {random_quotes['author']}"
+        
+        # Post the tweet
         client.create_tweet(text=quote)
         print("Successful sent tweet")
     
     except tweepy.TweepyException as e:
+        # Print error message if tweeting fails
         print(f"Error tweeting {e}")
      
 if __name__ == "__main__":
